44dd8cdee50a4cee36be798fdabaf1668fe04585
TorresX9-11 <emanuelleandrotorrestroncoso@gmail.com>
2025-06-29 17:22:08 -0400

    api terminada funcional, testeada con postman.

--- validators/movieValidators.js
+++
@@ -1,7 +0,0 @@
-// VALIDADORES DE MOVIE DESACTIVADOS POR CAMBIO DE ENFOQUE A MOTOS
-const Joi = require('joi');
-exports.movieSchema = Joi.object({
-title: Joi.string().required(),
-year: Joi.number().integer().required(),
-genre: Joi.string().optional()
-})
